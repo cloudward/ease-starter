2b4486e123a8fcb83cb8cb6f0ee19801a48da880
Lucas Simmons <lucas@cloudward.com>
2013-11-21 21:02:29 -0800

    Adding support API call back to ease-installer site

--- logs.php
+++ logs.php
@@ -1,5 +1,5 @@
 <?php
-
+error_reporting(E_ALL);
 require_once 'google/appengine/api/log/LogService.php';
 require_once 'google/appengine/util/string_util.php';
 
@@ -10,8 +10,8 @@ require_once('ease/core.class.php');
 $ease_core = new ease_core();
 
 $notification_email = trim($ease_core->process_ease('<# apply webstyle.fb7fbd7a3bed0f0c9cf0180d26a6d9c1 as "webstyle" .#><#[webstyle.notification_site_email]#>',true));
-
-date_default_timezone_set('Australia/Sydney');
+$has_style = true;
+//date_default_timezone_set('Australia/Sydney');
 session_start();
 $notification_email = "";
 if($_SESSION['keypassn'] != "554bd8fc3801fd2e560154e42a32670ab554bd8fc3801fd2e560154e42a32670ab" && $notification_email){
@@ -31,11 +31,11 @@ if(!$notification_email){
   echo "No email address found";
   exit;
 }
-$application_id = str_replace("~s","",$_SERVER['APPLICATION_ID']);
+$application_id = str_replace("s~","",$_SERVER['APPLICATION_ID']);
 
 // Get time range from querystrings
-$start = (float) $_GET["start"];
-$end = (float) $_GET["end"];
+$start = (float) strtotime("-1 day");
+$end = (float) strtotime("now");
 
 // Each level corresponds to the message with the same index.
 $levels = explode(' ', $_GET['levels']);
@@ -61,6 +61,8 @@ if (isset($_GET['min'])) {
 }
 
 $options = [
+    'start_time' => $start * 1e6,
+  'end_time' => $end * 1e6,
   'include_app_logs' => true
 ];
 $logs = LogService::fetch($options);
@@ -117,16 +119,19 @@ foreach ($logs as $log) {
       //  die('Level mismatch. Expected: ' . $gaeLevel .
       //      ' Got: ' . $app_log->getLevel());
       //}
-      $message_text .= $app_log->getMessage() . "";
-      $matches[] = $app_log->getMessage();
       
+      if($app_log->getMessage()){
+        $message_text .= $app_log->getMessage() . "";
+        $matches[] = $app_log->getMessage();
+      }else{
+        
+      }
     }
     
-    $message[$application_id][] = $message_text;
-    $full_message_text .= htmlspecialchars($message_text) . "<BR><BR>";
+    $message[$application_id][] = htmlspecialchars($message_text,ENT_QUOTES);
+    $full_message_text .= $message_text . "<BR><BR>";
   }
-  
-  
+
   //var_dump(json_encode($message));
 
   $full_message_text = "Application ID: " . $application_id . "<BR>Submitted by: " . htmlspecialchars($_POST['support_contact_email']) . "<BR>Problem: " . htmlspecialchars($_POST['support_desc']) . "<BR><BR>" . $full_message_text;
@@ -141,18 +146,25 @@ foreach ($logs as $log) {
 		
 	#>';
   $ease_core->process_ease($ease_code,true);
+  
+  $problem = "Submitted by: " . htmlspecialchars($_POST['support_contact_email']) . "<BR>Problem: " . htmlspecialchars($_POST['support_desc']);
+$data = array('email_address' => $notification_email, 'application_id' => $application_id, 'problem' => $problem, 'json_logs' => json_encode($message,ENT_QUOTES));
+$data = http_build_query($data);
+$context =
+    array("http"=>
+      array(
+        "method" => "post",
+        "content" => $data
+      )
+    );
+$context = stream_context_create($context);
+$result = file_get_contents("http://www.ease-installer.com/api/index.php", false, $context);
 
+ 
 if(!$has_style){
-header("Location: /?page=___deleteaftersetup_support_received");
+header("Location: /?page=___deleteaftersetup_support_received&message=$result");
 }else{
-header("Location: /?page=admin_support_received");
+header("Location: /?page=admin_support_received&message=$result");
 }
 exit;
-//// Finish if we have read all expected messages.
-//if ($matches === $messages) {
-//  echo 'PASS';
-//} else {
-//  die('Expected messages:' . print_r($messages, true) . "\n" .
-//      'Found:' . print_r($matches, true));
-//}
 ?>
